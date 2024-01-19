3e9c103e2065a90fe44644e63a67c334cb8f9890
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-01-19 10:18:27 +0530

    Added Episode 03 header assignment

--- App.js
+++ App.js
@@ -11,11 +11,7 @@ const Header = () => {
   return (
     <div className="main">
       <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.creativefabrica.com/wp-content/uploads/2020/11/09/Lion-Head-Logo-Vector-Graphics-6532442-1-1-580x387.jpg"
-          alt="Logo"
-        />
+        <img className="logo" src={require("./public/logo.png")} alt="Logo" />
       </div>
       <div className="search-bar">
         <input type="text" placeholder="Search" />
