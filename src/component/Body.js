36087a693fefa68a90f85b71bf3c078b36a61e9a
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-01-20 22:04:52 +0530

    Added Episode 05 of namaste react

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { resObj } from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resObj);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.8744775&lng=75.37036619999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   const filterListOFRestaurant = (restaurants) =>
     restaurants.filter((res) => res.info.avgRating >= 4.2);
 
-  return (
+  //conditional rendering
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
